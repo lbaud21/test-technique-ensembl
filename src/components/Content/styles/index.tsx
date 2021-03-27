087647a3ffed17f782291fb08bb4e9d0e498f242
Lucas <lucas.baudino@outlook.fr>
2021-03-27 22:45:11 +0100

    Content component creation

---
+++ src/components/Content/styles/index.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const ContentSectionWrapper = styled.div`
+  width: 80%;
+  display: flex;
+  flex-flow: row nowrap;
+`;
+
+export const ContentElementWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  width: 15%;
+  height: 30%;
+`;
+
+export const ContentElementImage = styled.img`
+  width: 100%;
+`;
+
+export const ContentElementTitle = styled.h3`
+  font-size: 0.5em;
+  text-align: center;
+`;
