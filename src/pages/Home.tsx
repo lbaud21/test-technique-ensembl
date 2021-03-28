bcd0c24f2f717ee4ebc3b26293f5308be396e3b9
Lucas <lucas.baudino@outlook.fr>
2021-03-28 20:15:16 +0200

    adding content container to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Header from "containers/Header/Header";
+import Content from "containers/Content/HomeContent";
 
 const Home = () => {
   return (
     <>
       <Header title="Popular Titles" />
+      <Content />
     </>
   );
 };
