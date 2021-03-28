419731778ead1dedba24f9a4b102fcb4fb9a5232
Lucas <lucas.baudino@outlook.fr>
2021-03-28 21:36:14 +0200

    icons style

--- src/components/Footer/styles/index.ts
+++ src/components/Footer/styles/index.ts
@@ -20,3 +20,42 @@ export const CopyrightText = styled.p`
 color: white;
 font-size: 0.5em;
 `
+
+export const IconsWrapper = styled.div`
+display: flex;
+flex-flow: row nowrap;
+justify-content: space-between;
+align-items: center;
+`
+const IconsGroup = styled.div`
+display: flex;
+flex-flow: row nowrap;
+justify-content: space-evenly;
+align-items: center;
+`
+
+export const SocialNetworkIconsWrapper = styled(IconsGroup)`
+`
+interface SocialNetworkIconProps {
+backgroundImage : string
+}
+
+export const Icon = styled.a<SocialNetworkIconProps>`
+outline: none;
+background-image: url(${(props) => props.backgroundImage});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%;
+`
+
+export const SocialNetworkIcon = styled(Icon)`
+width: 1em;
+`
+
+export const StoreIconsWrapper = styled(IconsGroup)`
+
+`
+
+export const StoreIcon = styled(Icon)`
+width: 3em;
+`
