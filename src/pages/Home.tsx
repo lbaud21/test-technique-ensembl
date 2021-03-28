9be1c6fdd21846d282f05fdf51661e6d44f4c18b
Lucas <lucas.baudino@outlook.fr>
2021-03-28 21:33:44 +0200

    Adding Footer container

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "containers/Header/Header";
 import Content from "containers/Content/HomeContent";
+import Footer from "containers/Footer/Footer";
 
 const Home = () => {
   return (
     <>
       <Header title="Popular Titles" />
       <Content />
+      <Footer />
     </>
   );
 };
