f056a2da540a85ffcb92ffbee92ca78b2ab01433
Lucas <lucas.baudino@outlook.fr>
2021-03-28 21:35:08 +0200

    Icons component

---
+++ src/components/Footer/Icons.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  IconsWrapper,
+  SocialNetworkIconsWrapper,
+  SocialNetworkIcon,
+  StoreIconsWrapper,
+  StoreIcon,
+} from "./styles";
+
+const Icons = () => {
+  return (
+    <IconsWrapper>
+      <SocialNetworkIconsWrapper>
+        <SocialNetworkIcon backgroundImage="" />
+        <SocialNetworkIcon backgroundImage="" />
+        <SocialNetworkIcon backgroundImage="" />
+      </SocialNetworkIconsWrapper>
+
+      <StoreIconsWrapper>
+        <StoreIcon backgroundImage="" />
+        <StoreIcon backgroundImage="" />
+        <StoreIcon backgroundImage="" />
+      </StoreIconsWrapper>
+    </IconsWrapper>
+  );
+};
+
+export default Icons;
