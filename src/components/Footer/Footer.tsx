90f2f7f11f39718e2a49e19271fa53b1b8bfbf7d
Lucas <lucas.baudino@outlook.fr>
2021-03-28 21:37:07 +0200

    footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Helpers from "./Helpers";
+import Copyright from "./Copyright";
+import Icons from "./Icons";
+
+const Footer = () => {
+  return (
+    <>
+      <Helpers />
+      <Copyright />
+      <Icons />
+    </>
+  );
+};
+
+export default Footer;
