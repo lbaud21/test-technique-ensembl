6b2f3a252d092c2e420e9260c110774eede1b9b4
Lucas <lucas.baudino@outlook.fr>
2021-03-29 02:30:06 +0200

    responsive styling

--- src/components/Content/styles/index.tsx
+++ src/components/Content/styles/index.tsx
@@ -1,15 +1,35 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const ContentSection = styled.section`
-  min-height: 80vh;
-  padding: 5% 10%;
+export const ErrorMessage = styled.p`
+  color: black;
 `;
 
-export const ContentSectionWrapper = styled.div`
-  width: 80%;
+export const ContentSection = styled.section`
+  min-height: 40vh;
+  padding: 5% 10%;
+  padding-right: 1.5%;
+
+  @media screen and (max-width: 800px) {
+    padding: 5% 2%;
+  }
+`;
+
+export const HomeContentSectionWrapper = styled.div`
   display: flex;
-  flex-flow: row nowrap;
+  flex-flow: row wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  @media screen and (max-width: 800px) {
+    justify-content: center;
+  }
+`;
+
+export const ProgramContentSectionWrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-evenly;
   align-items: flex-start;
 `;
 
@@ -20,20 +40,62 @@ export const ContentElementWrapperLink = styled(Link)`
   height: 40vh;
   margin-right: 2%;
   text-decoration: none;
+
+  @media screen and (max-width: 1000px) {
+    width: 20%;
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 30%;
+  }
+
+  @media screen and (max-width: 400px) {
+    width: 70%;
+  }
 `;
 
 export const ContentElementWrapper = styled.div`
   display: flex;
   flex-flow: column nowrap;
-  width: 15%;
+  width: 13%;
   height: 40vh;
-  margin-right: 2%;
   text-decoration: none;
+
+  @media screen and (max-width: 1000px) {
+    width: 20%;
+  }
+
+  @media screen and (max-width: 800px) {
+    width: 30%;
+    height: 60vh;
+  }
+
+  @media screen and (max-width: 700px) {
+    width: 45%;
+    height: 70vh;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 60%;
+    height: 70vh;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 80%;
+    height: 90vh;
+  }
+
+  @media screen and (max-width: 450px) {
+    width: 90%;
+    height: 100vh;
+  }
 `;
 
 interface ContentElementImageProps {
   backgroundImage: string;
   backgroundColor?: string;
+  backgroundPosition: string;
+  backgroundSize: string;
 }
 
 export const ContentElementImage = styled.div<ContentElementImageProps>`
@@ -41,23 +103,35 @@ export const ContentElementImage = styled.div<ContentElementImageProps>`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 90%;
+  height: 70%;
   background-image: url(${(props) => props.backgroundImage});
   background-color: ${(props) =>
     props.backgroundColor ? props.backgroundColor : "transparent"};
   background-repeat: no-repeat;
-  background-position: center;
-  background-size: 200%;
+  background-position: ${(props) => props.backgroundPosition};
+  background-size: ${(props) => props.backgroundSize};
+
+  @media screen and (max-width: 800px) {
+    background-size: cover;
+  }
 `;
 
 export const ContentElementImageText = styled.p`
   color: black;
   font-size: 1em;
   text-align: left;
-  margin: 0.25em;
+  margin: 0.5em;
+
+  @media screen and (max-width: 800px) {
+    font-size: 1.5em;
+  }
 `;
 
 export const ContentElementTitle = styled.h3`
   color: white;
   font-size: 2.5em;
+
+  @media screen and (max-width: 800px) {
+    font-size: 1.5em;
+  }
 `;
--- src/components/Footer/styles/index.ts
+++ src/components/Footer/styles/index.ts
@@ -1,36 +1,72 @@
 import styled from "styled-components"
 
 export const FooterSection = styled.footer`
-  height: 30vh;
+  height: 20vh;
   display: flex;
   flex-flow: column nowrap;
   background-color: rgba(30, 30, 30, 255);
+  padding: 5% 10%;
+
+  @media screen and (max-width: 650px){
+    padding: 2% 5%
+  }
+
 `;
 
 export const HelpersWrapper = styled.div`
 display: flex;
-flex-flow: row nowrap;
+flex-flow: row wrap;
+margin-bottom: 0.8em;
+
+@media screen and (max-width: 800px){
+  margin-bottom: 0.4em
+}
+
 `
 
 export const HelperItem = styled.a`
 color: white;
 font-size: 1em;
+
+@media screen and (max-width: 800px){
+  font-size: 0.7em;
+
+}
+
 `
 
 export const HelperSeparator = styled.p`
 color: white;
 font-size: 1em;
 margin: 0 0.5em;
+
+@media screen and (max-width: 800px){
+  font-size: 0.7em;
+
+}
+
 `
 
 export const CopyrightText = styled.p`
+margin: 0em;
+margin-bottom: 2.5em;
 color: white;
-font-size: 0.5em;
+font-size: 1em;
+
+@media screen and (max-width: 800px){
+  margin-bottom: 1em
+}
+
+@media screen and (max-width: 800px){
+  font-size: 0.7em;
+
+}
+
 `
 
 export const IconsWrapper = styled.div`
 display: flex;
-flex-flow: row nowrap;
+flex-flow: row wrap;
 justify-content: space-between;
 align-items: center;
 `
@@ -51,20 +87,50 @@ export const Icon = styled.a<SocialNetworkIconProps>`
 outline: none;
 background-image: url(${(props) => props.backgroundImage});
   background-repeat: no-repeat;
-  background-position: center;
+  background-position: left;
   background-size: contain;
+  margin: 0 0.5em;
 `
 
 export const SocialNetworkIcon = styled(Icon)`
 width: 2em;
 height: 2em;
+
+@media screen and (max-width: 800px){
+  width: 1.5em;
+height: 1.5em;
+}
+
+@media screen and (max-width: 550px){
+  width: 1em;
+  height: 1em;
+
+
+}
+
+
 `
 
 export const StoreIconsWrapper = styled(IconsGroup)`
-
 `
 
-export const StoreIcon = styled(Icon)`
-width: 6em;
-height: 2em;
+interface StoreIconsProps {
+  width?: string
+}
+
+export const StoreIcon = styled(Icon)<StoreIconsProps>`
+width: ${props => props.width || "9em"};
+height: 4em;
+
+@media screen and (max-width: 800px){
+  width: 6.5em;
+}
+
+@media screen and (max-width: 550px){
+  width: 5em;
+
+
+}
+
+
 `
