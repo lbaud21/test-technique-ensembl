b4be7c9c5c50230989d2590d130a6d3eadd7de5b
Lucas <lucas.baudino@outlook.fr>
2021-03-29 02:27:04 +0200

    movies page

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import Header from "components/Header/Header";
+import MoviesContainer from "containers/Content/MoviesContainer";
+import Footer from "components/Footer/Footer";
 
 const Movies = () => {
-    return (
-        <div>
-            Movies
-        </div>
-    )
-}
+  return (
+    <>
+      <Header title="Popular Movies" />
+      <MoviesContainer />
+      <Footer />
+    </>
+  );
+};
 
-export default Movies
+export default Movies;
