dc7d00b6dc53ffdb7ba74f5eeeccdbd487402137
Lucas <lucas.baudino@outlook.fr>
2021-03-29 02:31:40 +0200

    program content

--- src/components/Content/ProgramContent.tsx
+++ src/components/Content/ProgramContent.tsx
@@ -1,27 +1,62 @@
 import React from "react";
 import {
+  ErrorMessage,
   ContentSection,
   ContentElementWrapper,
   ContentElementImage,
-  ContentElementTitle,
-  ContentSectionWrapper,
+  ContentElementImageText,
+  ProgramContentSectionWrapper,
 } from "./styles";
 
-import useFetchProgram from "hooks/useFetchProgram";
+import { Program } from "hooks/types";
 
-const Content = () => {
-  const { loading, error, programList } = useFetchProgram("series");
+import Loader from "Loader/Loader";
+
+const ProgramContent = ({
+  loading,
+  error,
+  programList,
+}: {
+  loading: boolean;
+  error: boolean;
+  programList: Program[];
+}) => {
+  if (loading) {
+    return (
+      <ContentSection>
+        <ProgramContentSectionWrapper>
+          <Loader />
+        </ProgramContentSectionWrapper>
+      </ContentSection>
+    );
+  }
+
+  if (error) {
+    return (
+      <ContentSection>
+        <ProgramContentSectionWrapper>
+          <ErrorMessage>Oops, something went wrong</ErrorMessage>
+        </ProgramContentSectionWrapper>
+      </ContentSection>
+    );
+  }
 
   return (
     <ContentSection>
-      <ContentSectionWrapper>
-        <ContentElementWrapper>
-          {/* <ContentElementImage /> */}
-          <ContentElementTitle></ContentElementTitle>
-        </ContentElementWrapper>
-      </ContentSectionWrapper>
+      <ProgramContentSectionWrapper>
+        {programList.map((program) => (
+          <ContentElementWrapper key={program.title}>
+            <ContentElementImage
+              backgroundImage={program.images["Poster Art"].url}
+              backgroundPosition="top left"
+              backgroundSize="contain"
+            />
+            <ContentElementImageText>{program.title}</ContentElementImageText>
+          </ContentElementWrapper>
+        ))}
+      </ProgramContentSectionWrapper>
     </ContentSection>
   );
 };
 
-export default Content;
+export default ProgramContent;
